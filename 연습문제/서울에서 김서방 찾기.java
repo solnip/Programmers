35ee099d82e1cf57c6e6512df12c9dc7201eb076
Min <70986406+solnip@users.noreply.github.com>
2022-11-28 10:43:34 +0900

    lv. 1

---
+++ 연습문제/서울에서 김서방 찾기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] seoul) {
+        String answer = "";
+        int index = Arrays.asList(seoul).indexOf("Kim");
+        answer = "김서방은 " + index + "에 있다";
+        return answer;
+    }
+}
